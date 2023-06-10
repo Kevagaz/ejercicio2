a32ff6f1ddda3a0da48a10c3bb3205d7fc70ae80
Kevagaz <100064413+Kevagaz@users.noreply.github.com>
2023-06-10 20:11:08 +0200

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,55 @@
+
+int numerolf = 11;
+
+if (numerolf > 0) {
+    System.out.println("El número es positivo.");
+} else if (numeroolf < 0) {
+    System.out.println("El número es negativo.");
+} else {
+    System.out.println("El número es cero.");
+
+
+public class Main{
+     public static void main(String[] args) {
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+    }
+    public static void dowhile (String[] args) {
+        int numeroWhile2 = 0;
+         do {
+            System.out.println(numeroWhile2);
+            numeroWhile2 = numeroWhile2 + 1;
+         } while(numeroWhile2 < 3);
+    }
+    for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1){
+      System.out.println(numeroFor)  
+    }
+    
+    public static void name(String[] args) {
+        var estacion = "PRIMAVERA";
+
+        switch (estacion) {
+            case "VERANO":
+                System.out.println("es verano");
+            
+            case "PRIMAVERA":
+                System.out.println("es primavera");
+
+            case "OTONO":
+                System.out.println("es otono");
+
+            case "INVIERNO":
+                System.out.println("es invierno");
+            
+        
+            default:
+            System.out.println("esa no es una estacion")
+                break;
+        }
+    }
+}
+
